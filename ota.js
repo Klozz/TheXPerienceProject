ba2c0198b38ee42ba5880faca3d6c32148a793fc
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2016-11-17 17:20:51 -0600

    Update ota.js

--- ota.js
+++ ota.js
@@ -1,1 +1,1 @@
-
+{"error":null,"id":null,"result":[{"changes":"","channel":"NIGHTLY","filename":"xpe-11.1.0-20161117-a5ul-EXPERIMENTAL.zip","incremental":"264dbec018","md5sum":"cbb051b0f4d2ac5bdf407135f838f2d5","timestamp":"1479296579","url":"https://www.androidfilehost.com/?fid=457095661767116309"}]}
