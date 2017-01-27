d1265be5fa2c275bc52e85cc7c363dea5c73aed4
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2017-01-27 03:07:13 -0600

    test new js

--- ota.js
+++ ota.js
@@ -1,1 +1,14 @@
 {"error":null,"id":null,"result":[{"changes":"","channel":"NIGHTLY","filename":"xpe-11.1.0-20161117-a5ul-EXPERIMENTAL.zip","incremental":"264dbec018","md5sum":"cbb051b0f4d2ac5bdf407135f838f2d5","timestamp":"1479296579","url":"https://www.androidfilehost.com/?fid=457095661767116309"}]}
+
+{
+  "response": [
+    {
+      "datetime": 1485153889, 
+      "filename": "lineage-14.1-20170123-nightly-athene-signed.zip", 
+      "id": "5885b471a9156451add1f0e7", 
+      "romtype": "nightly", 
+      "url": "https://mirrorbits.lineageos.org/full/athene/20170123/lineage-14.1-20170123-nightly-athene-signed.zip", 
+      "version": "14.1"
+    }
+  ]
+}
